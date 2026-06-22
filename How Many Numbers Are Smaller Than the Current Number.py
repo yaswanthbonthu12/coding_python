36cd0c6b90c9631021c1a953ca1a0acc45d04c6f
yaswanthbonthu <yaswanthbonthucse@gmail.com>
2026-06-22 17:18:04 +0530

    Implement function to count smaller numbers

---
+++ How Many Numbers Are Smaller Than the Current Number.py
@@ -0,0 +1,8 @@
+       m=[]
+        for i in range(len(nums)):
+            count=0
+            for j in range(len(nums)):
+                if nums[i]>nums[j] and i!=j:
+                    count+=1
+            m.append(count)
+        return m
